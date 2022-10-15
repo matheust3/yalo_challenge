3b7beb7cb8f66cc193b371533f01eb47e62df5a6
Matheus Toniolli <matheus.toniolli@hotmail.com>
2022-10-15 20:29:08 +0000

    fix: replace ts-jest for swc

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,11 @@ const config = {
   "roots": ["<rootDir>/src"],
   "clearMocks": true,
   "collectCoverageFrom": ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**", '!<rootDir>/src/**/I*.ts', '!<rootDir>/src/**/index.ts'],
+  "coverageProvider": "babel",
   "coverageDirectory": "coverage",
   "testEnvironment": "node",
   "transform": {
-    ".+\\.ts$": "ts-jest"
+    ".+\\.ts$": "@swc/jest"
   }
 }
 
