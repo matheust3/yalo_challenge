a110c3187c20985030ca7d87e9f09dbd5a812ffa
Matheus Toniolli <matheus.toniolli@hotmail.com>
2022-10-16 23:21:05 +0000

    test: valid id_turma

--- src/main/routes/alunos-route.test.ts
+++ src/main/routes/alunos-route.test.ts
@@ -98,4 +98,14 @@ describe('alunos-route.test.ts - post', () => {
     expect(result.body).toEqual({ message: '"id_colegio" must be a positive number' })
     expect(result.status).toBe(400)
   })
+
+  test('ensure not create a aluno with invalid id_turma', async () => {
+    //! Arrange
+    aluno.id_turma = 0
+    //! Act
+    const result = await supertest(app).post('/api/alunos').send(aluno)
+    //! Assert
+    expect(result.body).toEqual({ message: '"id_turma" must be a positive number' })
+    expect(result.status).toBe(400)
+  })
 })
